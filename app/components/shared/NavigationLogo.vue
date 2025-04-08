dc45112a0cd1c63b24429ef9f201f256dd82866a
supebase <webappers@gmail.com>
2025-04-08 16:03:01 +0800

    chore: 更新版本号至0.7.7并升级@vueuse/nuxt依赖
    更新了package.json、package-lock.json和version.json中的版本号至0.7.7，并将@vueuse/nuxt依赖从13.0.0升级至13.1.0。同时，调整了NavigationLogo.vue中的样式类名，以保持一致性。

--- app/components/shared/NavigationLogo.vue
+++ app/components/shared/NavigationLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="eric -ml-1">
+  <div class="eric">
     <svg
       width="197"
       height="180"
@@ -11,19 +11,19 @@
         stroke="strokeColor"
         stroke-width="6.5"
         stroke-linejoin="round"
-        class="stroke-neutral-950 dark:stroke-neutral-50" />
+        class="stroke-neutral-900 dark:stroke-neutral-100" />
       <path
         d="M143.5 93.5C141.905 96.4507 141.957 98.0915 143.5 101"
         stroke="strokeColor"
         stroke-width="4.5"
         stroke-linejoin="round"
-        class="stroke-neutral-950 dark:stroke-neutral-50" />
+        class="stroke-neutral-900 dark:stroke-neutral-100" />
       <path
         d="M194.5 130C193.193 132.929 193.074 134.571 194.5 137.5"
         stroke="strokeColor"
         stroke-width="4.5"
         stroke-linejoin="round"
-        class="stroke-neutral-950 dark:stroke-neutral-50" />
+        class="stroke-neutral-900 dark:stroke-neutral-100" />
     </svg>
   </div>
 </template>
